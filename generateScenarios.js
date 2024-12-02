0059df292229d3ed9d22c138ad6a0d8605d583f9
istamosh <63596969+istamosh@users.noreply.github.com>
2024-12-02 08:49:52 +0000

    add: item list manager

---
+++ generateScenarios.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const scenariosDir = path.join(__dirname, "scenarios");
+const outputFilePath = path.join(__dirname, "src", "scenariosList.json");
+
+fs.readdir(scenariosDir, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error("Error reading scenarios directory:", err);
+    return;
+  }
+
+  const scenarios = files
+    .filter((file) => file.isDirectory())
+    .map((dir) => dir.name);
+
+  fs.writeFile(outputFilePath, JSON.stringify(scenarios, null, 2), (err) => {
+    err
+      ? console.error("Error writing scenarios list file:", err)
+      : console.log("Scenarios list generated successfully.");
+  });
+});
---
+++ src/app/Menu.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { Suspense, useState } from "react";
+import scenariosList from "../scenariosList.json";
+
+function Menu() {
+  const [scenario, setScenario] = useState(scenariosList[0] || "");
+  const [hasError, setHasError] = useState(false);
+
+  const loadScenario = async () => {
+    try {
+      const module = await import(`../../scenarios/${scenario}/App.tsx`);
+      return module;
+    } catch (error) {
+      setHasError(true);
+      throw error;
+    }
+  };
+
+  const ScenarioComponent = React.lazy(loadScenario);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Select Scenario</h1>
+        <select
+          onChange={(e) => {
+            setScenario(e.target.value);
+            setHasError(false);
+          }}
+          value={scenario}
+        >
+          {scenariosList.map((scenarioName, i) => (
+            <option key={i} value={scenarioName}>
+              {scenarioName}
+            </option>
+          ))}
+        </select>
+      </header>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        {hasError ? <div>File not found.</div> : <ScenarioComponent />}
+      </Suspense>
+    </div>
+  );
+}
+
+export default Menu;
